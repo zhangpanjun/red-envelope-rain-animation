f20c71d088f6131e5b25914453476895c17add8b
zhangpanjun <zhangpanjun93@163.com>
2025-01-14 17:27:01 +0800

    修复super()报错，将红包和slider背景图暂时替换为线上图片

--- index.js
+++ index.js
@@ -94,13 +94,12 @@ export default class RedEnvelopeRainAnimation {
 	#space = 300; // 生成红包间隔  最小300 加判断了
 	#speed_max = 5; // 红包下落速度随机值__最大值
 	#speed_min = 1; // 红包下落速度随机值__最小值
-	#red_img_src = './src/img/bag.png'; //红包图片
+	#red_img_src = 'http://www.makinokun.cn/assets/image/icon.jpg'; //红包图片
 	#red_img_ratio = 1; // 红包宽高比例
 	#duration = 5; // 红包雨下落时长
 	#activityTheme = '红包'; // 活动主题
 	#rainOverCallbackFun = () => {}; // 红包雨结束后回调
 	constructor(params) {
-		super();
 		let T = this;
 		T.#initParamsFromProps(T, params);
 	}
@@ -157,7 +156,7 @@ export default class RedEnvelopeRainAnimation {
                 border-radius: 50%;
                 left: 0;
                 top: 26px;
-                background-image: url("./src/img/slider-bg.png");
+                background-image: url("http://www.makinokun.cn/assets/image/icon.jpg");
                 background-size: cover;
                 font-family: Bebas;
                 font-weight: 400;
